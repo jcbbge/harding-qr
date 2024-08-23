642341097ba5985b73d79e04bc8da07045a21521
jrgantt <jr.gantt@icloud.com>
2024-08-23 19:43:50 -0400

    Adds 404 not found page. Needed basic page to render output and test router

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import { A } from "@solidjs/router";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you're looking for doesn't exist.</p>
+      <A href="/">Go back to the home page</A>
+    </div>
+  );
+};
+
+export default NotFound;

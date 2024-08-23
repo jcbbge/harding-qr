dfb9e8bcf0982cfce0293cbc682efdb790c75b1b
jrgantt <jr.gantt@icloud.com>
2024-08-22 20:55:38 -0400

    Adds additional new themes

--- src/components/interface/ThemeSwitcher.jsx
+++ src/components/interface/ThemeSwitcher.jsx
@@ -1,7 +1,17 @@
 import { For } from 'solid-js';
 import { useTheme } from '../../contexts/ThemeContext';
 
-const themes = ['digital-dawn', 'cyber-punk', 'retro-wave']; // Add more themes as needed
+const themes = [
+  'digital-dawn',
+  'cyber-punk',
+  'retro-wave',
+  'sand-martin',
+  'rail-pop',
+  'charge-back',
+  'echo-sphere',
+  'urban-pulse',
+  'modular-rhythm'
+];
 
 const ThemeSwitcher = () => {
   const { currentTheme, changeTheme } = useTheme();
@@ -9,7 +19,7 @@ const ThemeSwitcher = () => {
   return (
     <select value={currentTheme()} onChange={(e) => changeTheme(e.target.value)}>
       <For each={themes}>
-        {(theme) => <option value={theme}>{theme}</option>}
+        {(theme) => <option value={theme}>{theme.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}</option>}
       </For>
     </select>
   );

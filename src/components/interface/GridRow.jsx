8a4cf3f98c37bd164cffaa117dc65aae1d580e6b
jrgantt <jr.gantt@icloud.com>
2024-08-25 19:35:07 -0400

    Updates GridRow comp to accept/handle empty spaces

--- src/components/interface/GridRow.jsx
+++ src/components/interface/GridRow.jsx
@@ -1,22 +1,40 @@
-import { For } from 'solid-js';
+import { For, createMemo } from 'solid-js';
 import styles from './NameGrid.module.css';
 
+const accentVariables = ['accent-unexpected'];
+
 const GridRow = (props) => {
+  let colorIndex = 0;
+
+  const getNextAccentVariable = () => {
+    const variable = accentVariables[colorIndex];
+    colorIndex = (colorIndex + 1) % accentVariables.length;
+    return variable;
+  };
+
   return (
     <div class={styles.nameRow} style={{"--name-length": props.name.length}}>
       <For each={props.name}>
-        {(letterObj, letterIndex) => (
-          <div 
-            class={`${styles.letterBox} 
-                    ${letterObj.isVowel ? styles.vowel : ''} 
-                    ${letterObj.matched ? styles.matched : ''} 
-                    ${props.isActiveName && letterIndex() === props.activeVowelIndex ? styles.active : ''} 
-                    ${props.focusedPosition.row === props.rowIndex && letterIndex() === props.focusedPosition.col ? styles.focused : ''}`}
-            tabIndex={props.focusedPosition.row === props.rowIndex && letterIndex() === props.focusedPosition.col ? 0 : -1}
-          >
-            <span>{letterObj.currentLetter}</span>
-          </div>
-        )}
+        {(letterObj, letterIndex) => {
+          const accentVar = createMemo(() => 
+            letterObj.correctLetter === ' ' ? `--${getNextAccentVariable()}` : null
+          );
+
+          return (
+            <div 
+              class={`${styles.letterBox} 
+                      ${letterObj.isVowel ? styles.vowel : ''} 
+                      ${letterObj.matched ? styles.matched : ''} 
+                      ${props.isActiveName && letterIndex() === props.activeVowelIndex ? styles.active : ''} 
+                      ${props.focusedPosition.row === props.rowIndex && letterIndex() === props.focusedPosition.col ? styles.focused : ''}
+                      ${accentVar() ? styles.accentBackground : ''}`}
+              style={accentVar() ? { '--accent-var': `var(${accentVar()})` } : {}}
+              tabIndex={props.focusedPosition.row === props.rowIndex && letterIndex() === props.focusedPosition.col ? 0 : -1}
+            >
+              <span>{letterObj.currentLetter}</span>
+            </div>
+          );
+        }}
       </For>
     </div>
   );

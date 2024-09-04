f5cc5895c63ff297da1ab9dde8009f17a77c662d
jrgantt <jr.gantt@icloud.com>
2024-09-04 10:48:57 -0400

    code cleanup for theme stuff that was moved into context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { createSignal, onMount, onCleanup, ErrorBoundary } from 'solid-js';
-import { useTheme } from './contexts/ThemeContext';
 import NameGrid from './components/interface/NameGrid';
 import './App.css';
 
 function App() {
   const [company, setCompany] = createSignal('');
   const [role, setRole] = createSignal('');
-  const [timeLeft, setTimeLeft] = createSignal(60); // 60 seconds = 1 minute
-  const { theme, themes } = useTheme();
-
-  let timerInterval;
 
   onMount(() => {
     const urlPath = window.location.pathname;
@@ -19,33 +14,12 @@ function App() {
       setCompany(pathSegments[0]);
       setRole(pathSegments[1]);
     }
-
-    // Start the timer
-    timerInterval = setInterval(() => {
-      setTimeLeft(prev => {
-        if (prev <= 0) {
-          clearInterval(timerInterval);
-          return 0;
-        }
-        return prev - 1;
-      });
-    }, 1000);
-  });
-
-  onCleanup(() => {
-    clearInterval(timerInterval); // Clear the timer interval
   });
 
   function handleLetterUnlock() {
     // Handle letter unlock logic
+    console.log('Letter unlocked App.jsx callback');
   }
-
-  // Format time as MM:SS
-  const formattedTime = () => {
-    const minutes = Math.floor(timeLeft() / 60);
-    const seconds = timeLeft() % 60;
-    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-  };
 
   return (
     <ErrorBoundary
@@ -59,6 +33,9 @@ function App() {
           role={role()}
           onLetterUnlock={handleLetterUnlock}
         ></NameGrid>
+      </div>
+      <div class="bottom-column">
+        <p>Unlock Letters</p>
       </div>
     </ErrorBoundary>
   );

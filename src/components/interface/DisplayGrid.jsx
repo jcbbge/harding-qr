e335e0af327ce89297118260841e9adaeeb1bdbc
jrgantt <jr.gantt@icloud.com>
2024-10-14 19:40:49 -0500

    new display grid components. stripped down versions of the name grid without interacitvity

---
+++ src/components/interface/DisplayGrid.jsx
@@ -0,0 +1,45 @@
+import { For, Show } from 'solid-js';
+import styles from './DisplayGrid.module.css';
+
+const DisplayGrid = (props) => {
+  console.log("DisplayGrid props:", props);
+
+  const isVowel = (letter) => ['A', 'E', 'I', 'O', 'U'].includes(letter.toUpperCase());
+  const MAX_LENGTH = 8;
+
+  const padWord = (word) => {
+    const padding = ' '.repeat(Math.max(0, MAX_LENGTH - word.length));
+    return padding + word;
+  };
+
+  return (
+    <div class={styles.displayGrid}>
+      <For each={props.gridWords}>
+        {(word, nameIndex) => (
+          <>
+            <Show when={props.gridElements && props.gridElements[nameIndex()]}>
+              <div class={styles.displayContent}>{props.gridElements[nameIndex()]}</div>
+            </Show>
+            <div
+              class={styles.displayRow}
+              style={{ '--name-length': MAX_LENGTH }}
+            >
+              <For each={padWord(word)}>
+                {(letter) => (
+                  <div class={`${styles.displayLetter} ${styles.matched} ${isVowel(letter) ? styles.vowel : ''} ${letter === ' ' ? styles.emptyBox : ''}`}>
+                    <span>{letter !== ' ' ? letter : ''}</span>
+                  </div>
+                )}
+              </For>
+            </div>
+            <Show when={props.gridElements && nameIndex() === props.gridWords.length - 1}>
+              <div class={styles.displayContent}>{props.gridElements[props.gridElements.length - 1]}</div>
+            </Show>
+          </>
+        )}
+      </For>    
+    </div>
+  );
+};
+
+export default DisplayGrid;
